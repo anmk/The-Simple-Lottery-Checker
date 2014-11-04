140883f295977dc51e5f98bbe4cfa16271b813f7
Andrzej Kałowski <a.m.kalowski@gmail.com>
2014-11-04 16:24:57 +0100

    Add languages files

---
+++ app/config/languages/english.php
@@ -0,0 +1,74 @@
+<?php
+/*
+ * $Id: english.php, v 1.0
+ * The Simple Lottery Checker
+ * Copyright (c) 2014 Andrzej Kałowski
+ * http://lotek.kalowski.com
+ */
+ 
+// Global entries for the html tag
+define('HTML_PARAMS', 'lang="pl"');
+
+define('CHARSET', 'UTF-8');
+ 
+define('ENTER_CHOOSED_NUMBER', 'Enter choosed numbers');
+
+define('FIRST_CHOOSED_NUMBER', 'first:');
+define('SECOND_CHOOSED_NUMBER', 'second:');
+define('THIRD_CHOOSED_NUMBER', 'third:');
+define('FOURTH_CHOOSED_NUMBER', 'fourth:');
+define('FIFTH_CHOOSED_NUMBER', 'fifth:');
+define('SIXTH_CHOOSED_NUMBER', 'sixth:');
+
+define('ENTER_DRAWN_NUMBER', 'Enter drawn numbers');
+define('FIRST_DRAWN_NUMBER', 'first:');
+define('SECOND_DRAWN_NUMBER', 'second:');
+define('THIRD_DRAWN_NUMBER', 'third:');
+define('FOURTH_DRAWN_NUMBER', 'fourth:');
+define('FIFTH_DRAWN_NUMBER', 'fifth:');
+define('SIXTH_DRAWN_NUMBER', 'sixth:');
+
+define('CHOOSED_NUMBER', '-choosed number: ');
+define('DRAWN_NUMBER', '-drawn number: ');
+
+define('ENTER_ALL_NUMBER', 'Enter all fields please');
+
+define('CORRECT_FIRST_CHOOSED_NUMBER', 'Correct first choosed number please');
+define('CORRECT_SECOND_CHOOSED_NUMBER', 'Correct second choosed number please');
+define('CORRECT_THIRD_CHOOSED_NUMBER', 'Correct third choosed number pleaseę');
+define('CORRECT_FOURTH_CHOOSED_NUMBER', 'Correct fourth choosed number please');
+define('CORRECT_FIFTH_CHOOSED_NUMBER', 'Correct fifth choosed number please');
+define('CORRECT_SIXTH_CHOOSED_NUMBER', 'Correct sixth choosed number please');
+
+define('CORRECT_FIRST_DRAWN_NUMBER', 'Correct first drawn number please');
+define('CORRECT_SECOND_DRAWN_NUMBER', 'Correct second drawn number please');
+define('CORRECT_THIRD_DRAWN_NUMBER', 'Correct third drawn number please');
+define('CORRECT_FOURTH_DRAWN_NUMBER', 'Correct fourth drawn number please');
+define('CORRECT_FIFTH_DRAWN_NUMBER', 'Correct fifth drawn number please');
+define('CORRECT_SIXTH_DRAWN_NUMBER', 'Correct sixth drawn number please');
+
+define('LUCKY_NUMBERS', 'Your unsort outcome:');
+define('SORT_LUCKY_NUMBERS', 'Your outcomes:');
+
+define('GUESS', '"&nbsp&nbspoutcomes');
+define('YOU_HAVE', 'You have&nbsp&nbsp"');
+define('INFO', 'The application can target finding lotteries six-numbers results.');
+define('ERRORS', 'The author used to force for the application would be worked right. And he won`t be owed when few mistakes will be occur during the work.');
+define('CHECK_OUTCOME', 'The Simple Lottery Checker');
+define('CHECK', 'Check');
+define('LOGIN', 'Login');
+define('REGISTER', 'Create Account');
+define('MY_ACCOUNT', 'My Account');
+define('USER_NAME', 'User:');
+define('PASSWORD', 'Password:');
+define('CONFIRM_PASSWORD', 'Confirm password:');
+define('WELCOME', 'Welcome again ');
+define('CHECK_YOUR_OUTCOME', ', chetnie sprawdze Twoje trafienia ');
+define('IN_YOUR_ACCONT', ' in my account');
+define('LOGOUT', 'You`ve Logged Out of Lotek');
+
+define('CHECK_M', 'Check');
+define('LOGOUT_M', 'Logout');
+define('MY_ACCOUNT_M', 'My Account');
+define('LOGIN_M', 'Login');
+define('REGISTER_M', 'Create Account');
---
+++ app/config/languages/polish.php
@@ -0,0 +1,77 @@
+<?php
+/*
+ * $Id: polish.php, v 1.0
+ * The Simple Lottery Checker
+ * Copyright (c) 2014 Andrzej Kałowski
+ * http://lotek.kalowski.com
+ */
+
+// Global entries for the html tag
+define('HTML_PARAMS', 'lang="pl"');
+
+// UTF-8
+define('CHARSET', 'UTF-8');
+// 
+define('ENTER_CHOOSED_NUMBER', 'Wprowadź wytypowane liczby');
+
+define('FIRST_CHOOSED_NUMBER', 'pierwsza:');
+define('SECOND_CHOOSED_NUMBER', 'druga:');
+define('THIRD_CHOOSED_NUMBER', 'trzecia:');
+define('FOURTH_CHOOSED_NUMBER', 'czwarta:');
+define('FIFTH_CHOOSED_NUMBER', 'piąta:');
+define('SIXTH_CHOOSED_NUMBER', 'szósta:');
+
+define('ENTER_DRAWN_NUMBER', 'Wprowadź wylosowane liczby');
+define('FIRST_DRAWN_NUMBER', 'pierwsza:');
+define('SECOND_DRAWN_NUMBER', 'druga:');
+define('THIRD_DRAWN_NUMBER', 'trzecia:');
+define('FOURTH_DRAWN_NUMBER', 'czwarta:');
+define('FIFTH_DRAWN_NUMBER', 'piąta:');
+define('SIXTH_DRAWN_NUMBER', 'szósta:');
+
+define('CHOOSED_NUMBER', '-a wytypowana liczba: ');
+define('DRAWN_NUMBER', '-a wylosowana liczba: ');
+
+define('ENTER_ALL_NUMBER', 'Proszę wypełnić wszystkie pola');
+
+define('CORRECT_FIRST_CHOOSED_NUMBER', 'Proszę poprawić pierwszą wytypowaną liczbę');
+define('CORRECT_SECOND_CHOOSED_NUMBER', 'Proszę poprawić drugą wytypowaną liczbę');
+define('CORRECT_THIRD_CHOOSED_NUMBER', 'Proszę poprawić trzecią wytypowaną liczbę');
+define('CORRECT_FOURTH_CHOOSED_NUMBER', 'Proszę poprawić czwartą wytypowaną liczbę');
+define('CORRECT_FIFTH_CHOOSED_NUMBER', 'Proszę poprawić piątą wytypowaną liczbę');
+define('CORRECT_SIXTH_CHOOSED_NUMBER', 'Proszę poprawić szósta wytypowaną liczbę');
+
+define('CORRECT_FIRST_DRAWN_NUMBER', 'Proszę poprawić pierwszą wylosowaną liczbę');
+define('CORRECT_SECOND_DRAWN_NUMBER', 'Proszę poprawić drugą wylosowaną liczbę');
+define('CORRECT_THIRD_DRAWN_NUMBER', 'Proszę poprawić trzecią wylosowaną liczbę');
+define('CORRECT_FOURTH_DRAWN_NUMBER', 'Proszę poprawić czwartą wylosowaną liczbę');
+define('CORRECT_FIFTH_DRAWN_NUMBER', 'Proszę poprawić piątą wylosowaną liczbę');
+define('CORRECT_SIXTH_DRAWN_NUMBER', 'Proszę poprawić szóstą wylosowaną liczbę');
+
+define('LUCKY_NUMBERS', 'Twoje trafienia w kolejności:');
+define('SORT_LUCKY_NUMBERS', 'Twoje trafienia:');
+
+define('GUESS', '"&nbsp&nbsptrafienia');
+define('YOU_HAVE', 'Masz&nbsp&nbsp"');
+define('INFO', 'Aplikacja służy do sprawdzania wyników gier losowych, w których losowanych jest sześć cyfr.');
+define('ERRORS', 'Autor dołożył wszelkich starań, aby aplikacja działała właściwie, jednak nie ponosi on żadnej odpowiedzialności za ewentualne błędy mogące nastąpić w trakcie jej pracy.');
+define('CHECK_OUTCOME', 'Lotek - Sprawdź swoje trafienia');
+define('CHECK', 'Sprawdź');
+define('LOGIN', 'Logowanie');
+define('REGISTER', 'Utwórz konto');
+define('MY_ACCOUNT', 'Moje konto');
+define('USER_NAME', 'Użytkownik:');
+define('PASSWORD', 'Hasło:');
+define('CONFIRM_PASSWORD', 'Potwierdź hasło:');
+define('WELCOME', 'Witaj ponownie ');
+define('CHECK_YOUR_OUTCOME', ', chętnie sprawdzę Twoje trafienia ');
+define('IN_YOUR_ACCONT', ' w swoim koncie');
+define('LOGOUT', 'Wylogowano pomyślnie');
+
+define('CHECK_M', 'Sprawdź');
+define('LOGOUT_M', 'Wyloguj');
+define('MY_ACCOUNT_M', 'Moje konto');
+define('LOGIN_M', 'Zaloguj');
+define('REGISTER_M', 'Zarejestruj');
+
+
