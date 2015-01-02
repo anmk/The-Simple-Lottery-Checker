c83c93a8a374897c7a1b2503a649843b483e27ef
Andrzej Kałowski <a.m.kalowski@gmail.com>
2015-01-02 18:48:24 +0100

    Added AccountSpace instance to autoload

--- app/models/main.php
+++ app/models/main.php
@@ -1,9 +1,9 @@
 <?php
 /*
- * $Id: main.php, v 1.0
+ * $Id: main.php, v 1.1
  * The Simple Lottery Checker
- * Copyright (c) 2014 Andrzej Kałowski
- * http://lotek.kalowski.com
+ * @author Andrzej Kałowski
+ * @link http://lotek.kalowski.com
  */
 
 function loader($class){
@@ -16,3 +16,4 @@ $Transfer = new Transfer_m();
 $Authorization = new Authorization_m();
 $cNumbers = new Cnumber_m('cNumber1', 'cNumber2', 'cNumber3', 'cNumber4', 'cNumber5', 'cNumber6');
 $dNumbers = new Dnumber_m('dNumber1', 'dNumber2', 'dNumber3', 'dNumber4', 'dNumber5', 'dNumber6');
+$AccountSpace = new AccountSpace_m();
