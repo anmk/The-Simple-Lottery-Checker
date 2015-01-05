a2bd031e6c5b0a8eb34ad20823eef8208688bbca
Andrzej Kałowski <a.m.kalowski@gmail.com>
2015-01-05 23:51:12 +0100

    Added saved numbers

--- app/views/privacy_v.php
+++ app/views/privacy_v.php
@@ -1,41 +1,62 @@
 <?php
 /*
- * $Id: privacy_v.php, v 1.0
+ * $Id: privacy_v.php, v 1.1
  * The Simple Lottery Checker
- * Copyright (c) 2014 Andrzej Kałowski
- * http://lotek.kalowski.com
+ * @author Andrzej Kałowski
+ * @link http://lotek.kalowski.com
  */
 ?>
 
 <title>Privacy</title>
 <meta charset="utf-8">
 <link rel = "stylesheet" type = "text/css" href = "/public/css/style.css" />
+<link rel = "stylesheet" type = "text/css" href = "/app/init.php" />		
+	
+<div class="main">
+	<p align="center"><?php echo '<span class="textBrown">' . MY_ACCOUNT . " - " . $_SESSION['username'] . '</span>'; ?></p>
 			
-	<div class="main">
-	<p align="center"><?php echo '<span class="textBrown">' . MY_ACCOUNT . '</span>'; ?></p>
-
-		<form action="" method="post" id="login">
-           
-            <div class="main" align="center">
+		<div class="main" align="center">
+				
+			<form action="../number_i.php" name="loadNumbers" id="loadNumbers" method="post">
             	
-            	<table class="inmain" border="0" width="100%" cellspacing="0" cellpadding="2">
+				<div><table>	
+						<?php
+						$AccountSpace = new AccountSpace_m();
+						$Transfer = new Transfer_m();
+						if (!isset($userId)) $userId = '';
+						$AccountSpace->numbersId($AccountSpace->usersId($userId));
+						$AccountSpace->readNumbers();
 						
-                </table>
-            	
-	            <div>
+						for($x = 0; $x <= $AccountSpace->lineFactor-1; $x++) {	
+							for($y = 0; $y <= 7; $y++) {
+								if ($y < 1) {								
+									echo '<span class="privLeftN" >' . $AccountSpace->namesAndNumbers['n'.$y][$x] . '</span>&nbsp';
+								} else if ($y > 0 && $y < 7) {
+									echo '<span class="privLeft" >' . $AccountSpace->namesAndNumbers['n'.$y][$x] . '</span>&nbsp';
+								} else if ($y > 6) {
+									$Transfer->setInfo('loadNumbers', $AccountSpace->namesAndNumbers['n'.$y][$x]); 
+						?>
+					<input type="radio" name="loadNumbers" id="loadNumbers" class="privRight" value="<?php print $Transfer->getInfo('loadNumbers'); ?>" /><br>
+						<?php						
+								}
+							}
+						}						
+						?>
+					<input type="image" src="/public/img/enterNumberPl.png" class="buttonPrivacy" />	       	
+				</table></div>
+            	          	
+			</form>
+        		
+				<div>
 		            <?php
-       					$ColourAlert = $this->getColourAlert();
-       					if($ColourAlert != '') {
-       					echo '<span class="ColourAlert">' . $ColourAlert . '</span>';
-       					}
-  					?>
-  				</div>
-            	
-            </div>
-        </form>	  
- 
-    </div>
-                  
-</body>
-</html>
-
+	   					$ColourAlert = $this->getColourAlert();
+	   					if($ColourAlert != '') {
+	   					echo '<span class="ColourAlert">' . $ColourAlert . '</span>';
+	   					}
+					?>
+				</div>
+  				           	
+		</div>
+            
+</div>
+    
