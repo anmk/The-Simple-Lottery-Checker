f62102db27e404fe1ea8f4ec4005a394c3ea0f9e
Andrzej Kałowski <a.m.kalowski@gmail.com>
2015-01-06 00:36:33 +0100

    Version 1.1

--- app/privacy.php
+++ app/privacy.php
@@ -1,15 +1,19 @@
 <?php
 /*
- * $Id: privacy.php, v 1.0
+ * $Id: privacy.php, v 1.1
  * The Simple Lottery Checker
- * Copyright (c) 2014 Andrzej Kałowski
- * http://lotek.kalowski.com
+ * @author Andrzej Kałowski
+ * @link http://lotek.kalowski.com
  */
 
 include_once("init.php");
+include(DIR_CONF.FILE_DB);
+
 if($Authorization->logStatus() == FALSE) {
 $Transfer->setColourAlert('Dostęp zastrzeżony', 'redColour');	
 $Transfer->redirectLink("../login_i.php");
-} else {
-$Transfer->loadLink(DIR_VWS.FILE_PRIVACY_VIEW);
+} else if (isset($_POST['loadN'])){
+$Transfer->loadLink(DIR_VWS.FILE_NUMBER_VIEW);	
+} else {	
+$Transfer->loadLink(DIR_VWS.FILE_PRIVACY_VIEW);	
 }
