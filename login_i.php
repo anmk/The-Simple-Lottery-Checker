769e4d6265ec0ef7c389f11dd2f61149651b608d
Andrzej Kałowski <a.m.kalowski@gmail.com>
2015-01-19 02:13:33 +0100

    Update

--- login_i.php
+++ login_i.php
@@ -11,63 +11,54 @@
 <!DOCTYPE html>
 <html lang="pl">
 	<head>
-		<meta charset="utf-8">
-		<meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
-		<title>Lotek</title>
-		<meta name="description" content="">
-		<meta name="author" content="Andrzej Kałowski">
-		<meta name="viewport" content="width=device-width; initial-scale=1.0">
-		<link rel="shortcut icon" href="/favicon.ico">
-		<link rel="stylesheet" type="text/css" href="public/css/style.css" >
+		<meta charset="utf-8" />
+		<title>The Simple Lottery Checker</title>
+		<meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1" />
+		<meta name="viewport" content="width=device-width; initial-scale=1.0" />
+		<meta name="description" content="The Simple Lottery Checker" />
+		<meta name="author" content="Andrzej Kałowski" />
+		<link rel="stylesheet" href="public/css/style.css" />
 	</head>
 
 <body id="login_i">
-		
-<?php // header ?>
-<div id="header">
-	<p align="center"><?php echo '<span class="textGreen">' . CHECK_OUTCOME . '</span>'; ?></p>
-</div>
-
-<div id="wrap">
 	
-	<?php //start navigation bar ?>
-    <table border="0" cellspacing="0" cellpadding="0" align="center">
-        <td width="100%" align="center">
-			<ul id="menu">
-			    <li id="number-i"><a href="number_i.php"><?php echo CHECK_M; ?></a></li>
-			    <?php if($Authorization->logStatus() == TRUE)
-			    { ?>
-			    <li id="logout-i"><a href="logout_i.php"><?php echo LOGOUT_M; ?></a></li>	
-			    <li id="privacy-i"><a href="privacy_i.php"><?php echo MY_ACCOUNT_M; ?></a></li>	
-			    <?php } else { ?>
-				<li id="login-i"><a href="login_i.php"><?php echo LOGIN_M; ?></a></li>
-				<li id="register-i"><a href="register_i.php"><?php echo REGISTER_M; ?></a></li>
-				<?php } ?>
-			</ul>
-        </td>
-    </table>       
-	<?php //end navigation bar ?>
+	<div id="wrap">		
 
-	<?php // main-body ?>	
-	<div id="main-body">
-		<div id="content">
-			<div id="box">
-				<?php include_once(APP_PATH.FILE_LOGIN); ?>
-			</div>
-		</div>
-	</div>
-	<?php // main-body end ?>
+		<header>
+			<p class ="textGreen"><?php echo CHECK_OUTCOME; ?></p>
+			<nav>
+			    <table border="0" cellspacing="0" cellpadding="0" align="center">
+			        <td width="100%" align="center">
+						<ul id="menu">
+						    <li id="number-i"><a href="number_i.php"><?php echo CHECK_M; ?></a></li>
+						    <?php if($Authorization->logStatus() == TRUE)
+						    { ?>
+						    <li id="logout-i"><a href="logout_i.php"><?php echo LOGOUT_M; ?></a></li>	
+						    <li id="privacy-i"><a href="privacy_i.php"><?php echo MY_ACCOUNT_M; ?></a></li>	
+						    <?php } else { ?>
+							<li id="login-i"><a href="login_i.php"><?php echo LOGIN_M; ?></a></li>
+							<li id="register-i"><a href="register_i.php"><?php echo REGISTER_M; ?></a></li>
+							<?php } ?>
+						</ul>
+			        </td>
+			    </table>
+		    </nav>       
+		</header>
 
-</div> 
-<?php // wrap end ?>
+		<section id="main-body">
+			<article id="content">
+				<aside id="box">
+					<?php include_once(APP_PATH.FILE_LOGIN); ?>
+				</aside>
+			</article>
+		</section> <?php // main-body end ?>
 
-<?php // footer ?>
-<div id="footer">
-	<div align="center" id="textGrey">
-		<span class="textLightGrey"><?php echo INFO;?></span>
-		<p class="textLightGrey"><?php echo ERRORS;?></p>
-	</div>	
-</div>
+		<footer id="footer">	
+			<p class="textLightGrey"><?php echo INFO;?></p>
+			<p class="textLightGrey"><?php echo ERRORS;?></p>
+		</footer>
  	
+	</div> <?php // wrap end ?>
+ 		
 </body>
 </html>
