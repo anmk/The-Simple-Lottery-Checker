3dbf79e4482bdd843c505907c5c32165c6d0e5e3
Andrzej Kałowski <a.m.kalowski@gmail.com>
2015-02-11 01:41:05 +0100

    Small edition

--- number_i.php
+++ number_i.php
@@ -6,7 +6,7 @@
  * @link http://lotek.kalowski.com
  */
  ?>
- 
+
 <?php include_once("app/init.php"); ?>
 <!DOCTYPE html>
 <html lang="pl">
@@ -30,40 +30,40 @@
 		<div class="container-fluid">		
 		
 			<header id="hdr">
-		    	<h4 class="textGreen animated fadeInDown"><?php echo CHECK_OUTCOME; ?></h4>
-		        <div id="menu" class="navbar-fixed-top container-fluid">
-		            <div class="col-lg-12 col-md-12 col-sm-12 col-xs-12">
-		                <nav class="navbar navbar-default" role="navigation">
-		                    <div class="container-fluid">
-		                        <div class="navbar-header">
-		                            <button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#imp">
-		                                <span class="sr-only">navi</span>
-		                                <span class="icon-bar"></span>
-		                                <span class="icon-bar"></span>
-		                                <span class="icon-bar"></span>
-		                            </button>
-		                        </div>
-		                        <div class="collapse navbar-collapse" id="imp">
-		                            <ul class="nav navbar-nav">
-		                               <li class="active" id="number-i"><a href="number_i.php"><?php echo CHECK_M; ?></a></li>
-									    <?php if($Authorization->logStatus() == TRUE)
-									    { ?>
-									    <li id="privacy-i"><a href="privacy_i.php"><?php echo MY_ACCOUNT_M; ?></a></li>	
-									    <li id="logout-i"><a href="logout_i.php"><?php echo LOGOUT_M; ?></a></li>
-									    <?php } else { ?>
+				<h4 class="textGreen animated fadeInDown"><?php echo CHECK_OUTCOME; ?></h4>
+				<div id="menu" class="navbar-fixed-top container-fluid">
+					<div class="col-lg-12 col-md-12 col-sm-12 col-xs-12">
+						<nav class="navbar navbar-default" role="navigation">
+							<div class="container-fluid">
+								<div class="navbar-header">
+									<button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#imp">
+										<span class="sr-only">navi</span>
+										<span class="icon-bar"></span>
+										<span class="icon-bar"></span>
+										<span class="icon-bar"></span>
+									</button>
+								</div>
+								<div class="collapse navbar-collapse" id="imp">
+									<ul class="nav navbar-nav">
+										<li class="active" id="number-i"><a href="number_i.php"><?php echo CHECK_M; ?></a></li>
+										<?php if($Authorization->logStatus() == TRUE)
+										{ ?>
+										<li id="privacy-i"><a href="privacy_i.php"><?php echo MY_ACCOUNT_M; ?></a></li>	
+										<li id="logout-i"><a href="logout_i.php"><?php echo LOGOUT_M; ?></a></li>
+										<?php } else { ?>
 										<li id="login-i"><a href="login_i.php"><?php echo LOGIN_M; ?></a></li>
 										<li id="register-i"><a href="register_i.php"><?php echo REGISTER_M; ?></a></li>
 										<?php } ?>
-		                            </ul>
-		                        </div>
-		                    </div>
-		                </nav>
-		            </div>
-		        </div>
+									</ul>
+								</div>
+							</div>
+						</nav>
+					</div>
+				</div>
 				<ol class="breadcrumb">
 					<li class="active"><a href="number_i.php" class="breadcrumb" data-toggle="tooltip" data-placement="right" title="<?php echo CHECK_NUMBERS; ?>"><span class="glyphicon glyphicon-check"></span><?php echo '&nbsp' . CHECK; ?></a></li>
 				</ol>
-		    </header>
+			</header>
 		
 			<section class="row">
 				<div class="col-lg-3 col-md-2 col-sm-1 hidden-xs">
