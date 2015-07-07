2e61c7086c3658ddbee66518b2da5c81213c3f43
Andrzej Kałowski <a.m.kalowski@gmail.com>
2015-07-07 15:51:28 +0200

    Small edition

--- app/models/class.Authorization_m.php
+++ app/models/class.Authorization_m.php
@@ -11,26 +11,26 @@ class Authorization_m {
 	private $key = '';
 
 	function __construct(){
-    }
+	}
 	
 	function logGuard($user, $pass){
 		global $database;
 		$encryptPass = $this->encrypt($pass);
 			if($stmt = $database->prepare("SELECT username, password FROM users WHERE username = ? AND password = ?")){
 				$stmt->bind_param("ss", $user, $encryptPass);
-            	$stmt->execute();
-            	$stmt->store_result(); 
-            	if($stmt->num_rows > 0){
-                	$stmt->close();
-                	return TRUE;
-            	} else {
-                	$stmt->close();
-                	return FALSE;
-            	}
-        	} else {
-            die();
-        	}
-    }
+				$stmt->execute();
+				$stmt->store_result(); 
+				if($stmt->num_rows > 0){
+					$stmt->close();
+					return TRUE;
+				} else {
+					$stmt->close();
+					return FALSE;
+					}
+			} else {
+			die();
+			}
+	}
 	 
 	function filter($user){
 	  	global $database;
@@ -40,44 +40,44 @@ class Authorization_m {
 				$stmt->store_result();
 				if($stmt->num_rows == 0){	
 					$stmt->close();
-	                return TRUE;
-					} else {
+					return TRUE;
+				} else {
 					$stmt->close();
 					return FALSE;	
 					}
 			} else {
 			die();
 			}
-	 }
+	}
 	   
 	function register($user, $pass){
-	  	global $database;
+		global $database;
 			if($stmt = $database->prepare("INSERT INTO users (username, password) VALUES (?, ?)")){
 				$stmt->bind_param("ss", $user, $this->encrypt($pass));
 				$stmt->execute(); 
 				$stmt->close();
-                return TRUE;
-            	} else {
-                $stmt->close();
-                return FALSE;
+				return TRUE;
+			} else {
+				$stmt->close();
+				return FALSE;
 				}
-	  }
+	}
 											
 	function logStatus(){
-        if(isset($_SESSION['login'])){
-            return TRUE;
-        } else {
-            return FALSE;
-        }
-    }
+		if(isset($_SESSION['login'])){
+			return TRUE;
+		} else {
+			return FALSE;
+		}
+	}
 	
 	function logoff(){
-        session_destroy();
-        session_start();
-    }
+		session_destroy();
+		session_start();
+	}
     
-    function encrypt($pass){
+	function encrypt($pass){
 		return sha1($pass.$this->key);
-    }
+	}
     
 }
