27c7b49828839317672fad028cdecda2936e9208
Andrzej Kałowski <a.m.kalowski@gmail.com>
2015-07-07 16:10:42 +0200

    Update

--- app/models/class.AccountSpace_m.php
+++ app/models/class.AccountSpace_m.php
@@ -1,68 +1,68 @@
 <?php
 /*
- * $Id: class.AccountSpace_m.php, v 1.1
+ * $Id: class.AccountSpace_m.php, v 1.2
  * The Simple Lottery Checker
  * @author Andrzej Kałowski
- * @link http://lotek.kalowski.com
+ * @link http://projects.kalowski.com/lotterychecker/
  */
 
 class AccountSpace_m {
 		
 	private $user;
 	private $userId;
-	private $numberId;
 	private $numberName;	
+	public $numberId;
 	public $n1;
-    public $n2;
-    public $n3;
+	public $n2;
+	public $n3;
 	public $n4;
-    public $n5;
-    public $n6;
+	public $n5;
+	public $n6;
 	public $n7;
 	public $lineFactor;
 	public $numbersIdArray = array();
 	public $namesAndNumbers = array (
-			    	"n0" => array(),
-			    	"n1" => array(),
-			    	"n2" => array(),
-			    	"n3" => array(),
-			    	"n4" => array(),
-			    	"n5" => array(),
-			    	"n6" => array(),
-			    	"n7" => array(),
+					"n0" => array(),
+					"n1" => array(),
+					"n2" => array(),
+					"n3" => array(),
+					"n4" => array(),
+					"n5" => array(),
+					"n6" => array(),
+					"n7" => array(),
 					);
 	public $numbersWhenId = array();
 	
 	function __construct(){
-    }
+	}
 	
 	function logName($user){
 		if(isset($_SESSION['username'])){
 			$user = $_SESSION['username'];
 			return $user;
-        } else {
-        	$user = 'logedOff';
+		} else {
+			$user = 'logedOff';
 			return $user;
-        }
-    }
+		}
+	}
 	
 	function usersId($userId){
 		global $database;
-		 	if($stmt = $database->prepare("SELECT user_id FROM users WHERE username = ?")){
-				$stmt->bind_param("s", $_SESSION['username']);
-				$stmt->execute();
-				$stmt->bind_result($userId);
-				$stmt->fetch();
-					return $userId;
-					$stmt->close();	
-			} else {
-			$stmt->close();	
-			return FALSE;
-			}
-	 }
+		if($stmt = $database->prepare("SELECT user_id FROM users WHERE username = ?")){
+			$stmt->bind_param("s", $_SESSION['username']);
+			$stmt->execute();
+			$stmt->bind_result($userId);
+			$stmt->fetch();
+				return $userId;
+				$stmt->close();	
+		} else {
+		$stmt->close();	
+		return FALSE;
+		}
+	}
 	
 	function numbersId($userId){
-	  	global $database;
+		global $database;
 		if($stmt = $database->prepare("SELECT number_id FROM numbers_to_users WHERE  user_id = ?")){
 			$stmt->bind_param("i", $userId);
 			$stmt->execute();
@@ -75,46 +75,46 @@ class AccountSpace_m {
 		} else {
 		die();
 		}
-	 }
+	}
 	
 	function readNumbers(){
-	  	global $database;
-			$stmt = $database->prepare("SELECT number_name, n1, n2, n3, n4, n5, n6, number_id FROM numbers WHERE number_id = ?");
-				$this->lineFactor = 0;
-				foreach($this->numbersIdArray as $n => $value){
-					$stmt->bind_param('i', $value);
-					$stmt->execute();
-					$stmt->bind_result($numberName, $n1, $n2, $n3, $n4, $n5, $n6, $numberId);
-					while ($stmt->fetch()) {
-						array_push($this->namesAndNumbers["n0"],$numberName);
-						array_push($this->namesAndNumbers["n1"],$n1);
-						array_push($this->namesAndNumbers["n2"],$n2);
-						array_push($this->namesAndNumbers["n3"],$n3);
-						array_push($this->namesAndNumbers["n4"],$n4);
-						array_push($this->namesAndNumbers["n5"],$n5);
-						array_push($this->namesAndNumbers["n6"],$n6);
-						array_push($this->namesAndNumbers["n7"],$numberId);														
-					}
-					$this->lineFactor++;
+		global $database;
+		$stmt = $database->prepare("SELECT number_name, n1, n2, n3, n4, n5, n6, number_id FROM numbers WHERE number_id = ?");
+			$this->lineFactor = 0;
+			foreach($this->numbersIdArray as $n => $value){
+				$stmt->bind_param('i', $value);
+				$stmt->execute();
+				$stmt->bind_result($numberName, $n1, $n2, $n3, $n4, $n5, $n6, $numberId);
+				while ($stmt->fetch()) {
+					array_push($this->namesAndNumbers["n0"],$numberName);
+					array_push($this->namesAndNumbers["n1"],$n1);
+					array_push($this->namesAndNumbers["n2"],$n2);
+					array_push($this->namesAndNumbers["n3"],$n3);
+					array_push($this->namesAndNumbers["n4"],$n4);
+					array_push($this->namesAndNumbers["n5"],$n5);
+					array_push($this->namesAndNumbers["n6"],$n6);
+					array_push($this->namesAndNumbers["n7"],$numberId);														
 				}
-			$stmt->close();
+				$this->lineFactor++;
+			}
+		$stmt->close();
 	}
 
 	function loadNumbers($numberId){
-	  	global $database;
-			$stmt = $database->prepare("SELECT n1, n2, n3, n4, n5, n6 FROM numbers WHERE number_id = ?");
-					$stmt->bind_param('i', $numberId);
-					$stmt->execute();
-					$stmt->bind_result($n1, $n2, $n3, $n4, $n5, $n6);
-					$stmt->fetch();
-					$stmt->close();
-						array_push($this->numbersWhenId,$n1);
-						array_push($this->numbersWhenId,$n2);
-						array_push($this->numbersWhenId,$n3);
-						array_push($this->numbersWhenId,$n4);
-						array_push($this->numbersWhenId,$n5);
-						array_push($this->numbersWhenId,$n6);
-					return $this->numbersWhenId;
+		global $database;
+		$stmt = $database->prepare("SELECT n1, n2, n3, n4, n5, n6 FROM numbers WHERE number_id = ?");
+				$stmt->bind_param('i', $numberId);
+				$stmt->execute();
+				$stmt->bind_result($n1, $n2, $n3, $n4, $n5, $n6);
+				$stmt->fetch();
+				$stmt->close();
+					array_push($this->numbersWhenId,$n1);
+					array_push($this->numbersWhenId,$n2);
+					array_push($this->numbersWhenId,$n3);
+					array_push($this->numbersWhenId,$n4);
+					array_push($this->numbersWhenId,$n5);
+					array_push($this->numbersWhenId,$n6);
+				return $this->numbersWhenId;
 	}
 
 	function remember($numberId, $numberName, $n1, $n2, $n3, $n4, $n5, $n6){
@@ -123,7 +123,7 @@ class AccountSpace_m {
 		if ($stmt = $database->prepare($query)){
 			$stmt->bind_param('siiiiii', $numberName, $n1, $n2, $n3, $n4, $n5, $n6);
 			if($stmt->execute()){
-		    	$this->numberId = $stmt->insert_id;
+				$this->numberId = $stmt->insert_id;
 				return $numberId;
 				$stmt->close();	
 			}else{
@@ -138,9 +138,10 @@ class AccountSpace_m {
 	
 	function numbersToUsers($numberId, $userId){
 		global $database;
+		$saveUserId = $this->usersId($userId);
 		$query = "INSERT INTO numbers_to_users (number_id, user_id) VALUES(?, ?)";
 			if ($stmt = $database->prepare($query)){
-				$stmt->bind_param('ii', $this->numberId, $this->usersId($userId));
+				$stmt->bind_param('ii', $this->numberId, $saveUserId);
 				$stmt->execute();
 				$stmt->fetch();
 				$stmt->close();
